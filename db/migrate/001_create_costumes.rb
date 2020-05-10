e3352957a956a53b19e0a40aea167798fec972e4
C. Leow <chienleow1@gmail.com>
2020-05-10 03:56:39 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table :artists do |t|
+    create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.string :size
