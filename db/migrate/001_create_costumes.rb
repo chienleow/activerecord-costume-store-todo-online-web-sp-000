ff7582b6c76a72e4c4fa7478a60a0dfeab2373ea
C. Leow <chienleow1@gmail.com>
2020-05-10 03:53:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
-
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
 end
 
 # Create a class and inherit from ActiveRecord::Migration
